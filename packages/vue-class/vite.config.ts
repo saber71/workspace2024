fcdce07c3e9c0bb9c7dfebbbdf2005aed88cc868
saber71 <1533357473@qq.com>
2024-02-05 14:24:07 +0800

    chore(vue-class): Initial commit

---
+++ packages/vue-class/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import dtsPlugin from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [vue(), vueJsx(), dtsPlugin({ rollupTypes: true })],
+  esbuild: { keepNames: true },
+  build: {
+    rollupOptions: {
+      external: ["vue"],
+    },
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es"],
+    },
+  },
+});
