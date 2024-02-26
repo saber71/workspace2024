2e2589c47e9d01280adafe11c7d92dab81fae319
saber71 <1533357473@qq.com>
2024-02-26 19:52:15 +0800

    feat(dependency-injection): Split Container to get LoadableContainer

--- packages/dependency-injection/types.d.ts
+++ packages/dependency-injection/types.d.ts
@@ -8,9 +8,18 @@ declare interface ContainerMember {
   /* 访问时优先返回该值，若无值将依次尝试factory、getter */
   value?: any;
   /* 每次访问时都会执行生成一个新值 */
-  factory?: () => any;
+  factory?: (...args: any[]) => any;
   /* 懒加载，只在第一次访问的时候执行 */
   getter?: () => any;
   /* getter生成的值会缓存在这里 */
   getterValue?: any;
+}
+
+/* 加载被装饰器Injectable装饰过的内容时可用的选项 */
+declare interface LoadOption {
+  /* 指定要被加载的模块，与装饰器Injectable入参中的moduleName相同 */
+  moduleName?: string;
+
+  /* 默认true。是否让子类覆盖父类的实例。如果为true，则通过父类名字拿到的，并非是其自身的实例，而是子类的实例 */
+  overrideParent?: boolean;
 }
