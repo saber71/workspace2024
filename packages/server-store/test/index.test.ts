67cba87e1473e5162409720ddf3eb3db933aef1a
saber71 <1533357473@qq.com>
2024-03-26 16:17:48 +0800

    test(store): Improve test cases

--- packages/server-store/test/index.test.ts
+++ packages/server-store/test/index.test.ts
@@ -28,6 +28,58 @@ describe("server-store", () => {
     expect(queryResult.length).toEqual(1);
     expect(queryResult[0].age).toEqual(20);
   });
+  test.concurrent("server-store-fs transaction add", async () => {
+    const store = await ServerStore.create(
+      createServerStoreFS(path.resolve(".", "store"), false),
+    );
+    const collection = store.collection<User>("user");
+    await collection
+      .transaction(async () => {
+        await collection.add(
+          {
+            name: "user",
+            password: "password",
+            age: 20,
+          },
+          {
+            name: "user1",
+            password: "password",
+            age: 200,
+          },
+        );
+        throw new Error();
+      })
+      .catch((e) => {});
+    const allData = await collection.getAll();
+    expect(allData.length).toEqual(0);
+  });
+  test.concurrent("server-store-fs transaction delete", async () => {
+    const store = await ServerStore.create(
+      createServerStoreFS(path.resolve(".", "store"), false),
+    );
+    const collection = store.collection<User>("user");
+    await collection.add(
+      {
+        name: "user",
+        password: "password",
+        age: 20,
+      },
+      {
+        name: "user1",
+        password: "password",
+        age: 200,
+      },
+    );
+    await collection
+      .transaction(async () => {
+        const deleted = await collection.delete({ name: "user1" });
+        expect(deleted.length).toEqual(1);
+        throw new Error();
+      })
+      .catch((e) => {});
+    const allData = await collection.getAll();
+    expect(allData.length).toEqual(2);
+  });
 });
 
 interface User extends StoreItem {
