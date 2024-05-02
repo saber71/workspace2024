5881559e9d2370c1204e69a1581893d7b92b060f
saber71 <1533357473@qq.com>
2024-05-02 10:21:00 +0800

    fix(common): Update validChars range to exclude unnecessary character
    Adjusted the range in the validChars array generation to exclude the unnecessary
    character ':'.

--- packages/common/dist/index.js
+++ packages/common/dist/index.js
@@ -152,7 +152,7 @@ function getValue(val) {
 const validChars = [];
 for(let i = 65; i <= 90; i++)validChars.push(String.fromCharCode(i));
 for(let i = 97; i <= 122; i++)validChars.push(String.fromCharCode(i));
-for(let i = 48; i <= 58; i++)validChars.push(String.fromCharCode(i));
+for(let i = 48; i <= 57; i++)validChars.push(String.fromCharCode(i));
 function randomString(len) {
     let result = "";
     for(let i = 0; i < len; i++){
