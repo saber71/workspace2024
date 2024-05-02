983c411c31234bb4c778ab4b867fa012fad7d6a0
saber71 <1533357473@qq.com>
2024-05-02 10:21:20 +0800

    fix(common): Update validChars range to exclude unnecessary character (#34)
    Adjusted the range in the validChars array generation to exclude the unnecessary
    character ':'.

--- packages/common/src/random.ts
+++ packages/common/src/random.ts
@@ -2,7 +2,7 @@ const validChars: string[] = [];
 
 for (let i = 65; i <= 90; i++) validChars.push(String.fromCharCode(i));
 for (let i = 97; i <= 122; i++) validChars.push(String.fromCharCode(i));
-for (let i = 48; i <= 58; i++) validChars.push(String.fromCharCode(i));
+for (let i = 48; i <= 57; i++) validChars.push(String.fromCharCode(i));
 
 export function randomString(len: number) {
   let result = "";
